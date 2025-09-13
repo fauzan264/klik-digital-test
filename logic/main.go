a9fe6bfa4c65daaff389afe4dd31ef068699e276
Fauzan <fauzan.c4@gmail.com>
2025-09-13 10:44:48 +0700

    solve findDisappeared logic

--- logic/main.go
+++ logic/main.go
@@ -1,18 +1,62 @@
 package main
 
+import "fmt"
+
 func main() {
 	// 1. Name Validator
+	// fmt.Println(validName("I. Tri"))
+	// fmt.Println(validName("I. T. Septian"))
+	// fmt.Println(validName("Ivan T. Septian"))
+	// fmt.Println(validName("Ivan"))
+	// fmt.Println(validName("i. Tri"))
+	// fmt.Println(validName("I Tri"))
+	// fmt.Println(validName("I. Tri Septian"))
+	// fmt.Println(validName("I. Tri P."))
+	// fmt.Println(validName("Ivan. Tri Septian"))
 
-    // 2. Find All Numbers Disappeared in an Array
-		nums  := []int{4,3,2,7,8,2,3,1}
-		findDisappeared(nums)
-		
+  // 2. Find All Numbers Disappeared in an Array
+	fmt.Println(findDisappeared([]int{4,3,2,7,8,2,3,1}))
+	fmt.Println(findDisappeared([]int{1,1}))
 }
 
-func findDisappeared(nums []int) {
-	// var lost_number []int
-	// var exist_number []int
-	// var 
+// func validName(name string) bool {
+// 	// re := regexp.MustCompile(`^([A-Z]\.|[A-Z][a-z]+)(\s([A-Z]\.|[A-Z][a-z]+))*\s[A-Z][a-z]+$`)
+// 	// // (`^([A-Z]\.|[A-Z][a-z]+)(\s([A-Z]\.|[A-Z][a-z]+))*\s[A-Z][a-z]+$`)
+// 	// return re.MatchString(name)
+// 	return true
+// }
 
-	// fmt.Println(exist_number)
+func findDisappeared(nums []int) []int {
+	// var lost_number []int
+	var lengthNumber = len(nums)
+	var existNumbers []int
+	var missingNumbers []int
+	seen := make(map[int]bool)
+
+	for i := 1; i <= lengthNumber; i++ {
+		// fmt.Println(nums[i])
+		for _, val := range nums {
+			// fmt.Printf("%s == %s ? %b\n", i, val, i == val)
+			if i == val && !seen[val] {
+				existNumbers = append(existNumbers, val)
+				seen[val] = true
+			}
+		}
+	}
+
+	for i := 1; i <= lengthNumber; i++ {
+		found := false
+		for _, val := range existNumbers {
+			// fmt.Println(i == val)
+			if i == val {
+				found = true
+				break
+			}
+		}
+		if (!found) {
+			missingNumbers = append(missingNumbers, i)
+		}
+	}
+
+	return missingNumbers
 }

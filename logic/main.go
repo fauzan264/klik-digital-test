62fadf23dcc96dee23a7047292dac8fe553f94ef
Fauzan <fauzan.c4@gmail.com>
2025-09-13 08:42:52 +0700

    wip: wip task logic

--- logic/main.go
+++ logic/main.go
@@ -4,4 +4,15 @@ func main() {
 	// 1. Name Validator
 
     // 2. Find All Numbers Disappeared in an Array
+		nums  := []int{4,3,2,7,8,2,3,1}
+		findDisappeared(nums)
+		
+}
+
+func findDisappeared(nums []int) {
+	// var lost_number []int
+	// var exist_number []int
+	// var 
+
+	// fmt.Println(exist_number)
 }
